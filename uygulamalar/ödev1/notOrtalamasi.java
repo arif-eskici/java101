4ac7a5991c1b96b331ca33f3b84726e2524ae79c
Arif ESKİCİ <aaema4@gmail.com>
2022-08-25 13:52:11 +0300

    java101_notOrtalamasiHesaplama

--- uygulamalar/ödev1/notOrtalamasi.java
+++ uygulamalar/ödev1/notOrtalamasi.java
@@ -1,3 +1,12 @@
+/* ÖDEV1:
+Not Ortalaması Hesaplayan Program
+Java ile Matematik, Fizik, Kimya, Türkçe, Tarih, Müzik derslerinin sınav puanlarını kullanıcıdan alan ve ortalamalarını hesaplayıp ekrana bastırılan programı yazın.
+
+Aynı program içerisinde koşullu ifadeler kullanılarak, eğer kullanıcının ortalaması 60'dan büyük ise ekrana "Geçti" , küçük ise "Kaldı" yazsın.
+
+Not : If ve Else kullanılmayacak...
+ */
+
 package ödev1;
 
 // kullanıcıdan veri alabilmek için gereken Scanner sınıfını projemize dahil ettik.
