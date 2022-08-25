302fb5f9756ba2abfeeb708e778c8478522f89d8
Arif ESKİCİ <aaema4@gmail.com>
2022-08-25 13:55:20 +0300

    java101_kdvHesaplama

--- uygulamalar/ödev2/KDV_Hesaplama.java
+++ uygulamalar/ödev2/KDV_Hesaplama.java
@@ -1,3 +1,9 @@
+/* ÖDEV2:
+Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayıp ekrana bastıran programı yazın.
+
+Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
+ */
+
 package ödev2;
 
 // kullanıcıdan veri alabilmek için gereken Scanner sınıfını projemize dahil ettik.
